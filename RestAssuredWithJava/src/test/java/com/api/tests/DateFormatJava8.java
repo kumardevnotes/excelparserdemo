f1e29a5a21930997f95ec9fefea4294ae61e96bf
dvenkatvasu <dvenkatvasu@gmail.com>
2021-03-07 21:48:18 +0530

    added date class

--- RestAssuredWithJava/src/test/java/com/api/tests/DateFormatJava8.java
+++ RestAssuredWithJava/src/test/java/com/api/tests/DateFormatJava8.java
@@ -1,6 +1,6 @@
-package com.api.tests;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class DateFormatJava8 {
@@ -19,11 +19,15 @@ public class DateFormatJava8 {
 			System.out.println("dayOfWeek -  from Saturday " + localDate.getDayOfWeek().toString());
 			dateString = FOMATTER.format(localDate.minusDays(1));
 			System.out.println("dayOfWeek -  from Saturday: Date Format " + dateString);
+			
 			break;
 		case "SUNDAY":
 			System.out.println("dayOfWeek -  from Sunday " + localDate.getDayOfWeek().toString());
 			dateString = FOMATTER.format(localDate.minusDays(2));
 			System.out.println("dayOfWeek -  from Sunday: Date Format " + dateString);
+			LocalDateTime localDateTime = LocalDateTime.now();
+			FOMATTER = DateTimeFormatter.ofPattern("dd-MMM-yyyy hh:mma");
+			System.out.println("Date Format dd-MMM-yyyy hh:mm a " + FOMATTER.format(localDateTime));
 			break;
 		case "MONDAY":
 			System.out.println("dayOfWeek -  from Monday " + localDate.getDayOfWeek().toString());
